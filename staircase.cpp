4e54fc82cba03c67c5da94f35ee96ec09be3b099
kunal <ksingh112113114@gmail.com>
2022-11-23 21:57:25 +0530

    just git checkout -f

--- staircase.cpp
+++ staircase.cpp
@@ -12,12 +12,9 @@ int main(){
 	
 	for (int i = 0; i < n+1; ++i){
 		cout<<space*(n-i);
-		//enjoy guys
 		for (int j = 0; j < i; ++j){
 		
 		cout<<'#';
-		//dil se diff 
-			
 		}
 		cout<<"\n";
 	}
